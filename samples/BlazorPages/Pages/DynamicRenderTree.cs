aa29cc341685bb4f8b3d4f74dfd321f8381886f7
Ivan Pavluk <pavlukivan@outlook.com>
2019-04-26 12:57:08 +0700

    Fix invalid "sequence"

--- samples/BlazorPages/Pages/DynamicRenderTree.cs
+++ samples/BlazorPages/Pages/DynamicRenderTree.cs
@@ -8,17 +8,16 @@ namespace BlazorPages.Pages
     {
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            var seq = 0;
-            builder.OpenElement(seq, "table");
-            builder.OpenElement(++seq, "tbody");
+            builder.OpenElement(0, "table");
+            builder.OpenElement(1, "tbody");
 
             for (var row = 0; row < 3; row++)
             {
-                builder.OpenElement(++seq, "tr");
+                builder.OpenElement(2, "tr");
                 for (var col = 0; col < 3; col++)
                 {
-                    builder.OpenElement(++seq, "td");
-                    builder.AddAttribute(++seq, "class", "tictactoe-cell");
+                    builder.OpenElement(3, "td");
+                    builder.AddAttribute(4, "class", "tictactoe-cell");
                     builder.CloseElement();
                 }
 
